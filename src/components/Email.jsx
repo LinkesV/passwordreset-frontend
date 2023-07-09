8ad46a6ec60fad2e3076e5a2125777a32ea1b239
Linkes <linkesvarun@gmail.com>
2023-07-09 16:17:38 +0800

    DONE

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -3,7 +3,6 @@ import { useState } from 'react'
 import emailjs from "@emailjs/browser";
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { ToastContainer } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 import './styles/Email.css'
 
